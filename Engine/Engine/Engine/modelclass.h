94cc53a9f800017df4f57a6474b48102007c7d9b
aytona <christopher.aytona@gmail.com>
2016-10-09 23:54:51 -0400

    modified modelclass.h for lab4

--- Engine/Engine/Engine/modelclass.h
+++ Engine/Engine/Engine/modelclass.h
@@ -2,14 +2,20 @@
 
 #include <d3d11.h>
 #include <d3dx10math.h>
+#include "textureclass.h"
 
 class ModelClass
 {
 private:
-    struct VertexType
+    /*struct VertexType
     {
         D3DXVECTOR3 position;
         D3DXVECTOR4 color;
+    };*/
+    struct VertexType
+    {
+        D3DXVECTOR3 position;
+        D3DXVECTOR2 texture;
     };
 
 public:
@@ -23,12 +29,19 @@ public:
 
     int GetIndexCount();
 
+    ID3D11ShaderResourceView* GetTexture();
+
 private:
     bool InitializeBuffers(ID3D11Device*);
     void ShutdownBuffers();
     void RenderBuffers(ID3D11DeviceContext*);
 
+    bool LoadTexture(ID3D11Device*, WCHAR*);
+    void ReleaseTexture();
+
 private:
     ID3D11Buffer *m_vertexBuffer, *m_indexBuffer;
     int m_vertexCount, m_indexCount;
+
+    TextureClass* m_Texture;
 };
